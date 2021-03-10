aef8965ad1687d0eee6481493a2b2b53a6669890
ktaswell <ktaswell@bhavi.us>
2021-03-10 15:40:04 -0800

    Timers are headaches

--- core/thingSpeakHandler.m
+++ core/thingSpeakHandler.m
@@ -1,7 +1,8 @@
 classdef thingSpeakHandler
-    %ThingSpeakHandler - Performs read and write of data in a static
-    %context
-    %   Detailed explanation goes here
+    %ThingSpeakHandler - Performs read and write of data to ThingSpeak for
+    %dice Game
+    %
+    %   
     
     properties (Constant)
         chID = 1320909;
@@ -13,16 +14,22 @@ classdef thingSpeakHandler
         function [names, idx] = writeName(obj,playerName)
             data = obj.readData();
             names = data{1};
-            if(names == "")
+            if(isa(names, 'char'))
+                if(string(names) == "")
+                    names = playerName;
+                else
+                    names = strcat(names, ', ', playerName);
+                end
+            elseif(isa(names,'numeric') && isnan(names))
                 names = playerName;
-            else
-                names = strcat(names, ', ', playerName);
             end
             
             idx = length(split(names, ', '));
             
             data{1} = names;
-            thingSpeakWrite(obj.chID, 'WriteKey', obj.writeKey, data);
+            data{4} = 0;
+            data{5} = 0;
+            thingSpeakWrite(obj.chID, 'WriteKey', obj.writeKey, 'Values',data);
         end
         
         function [names, gameStart] = readNames(obj)
@@ -31,11 +38,13 @@ classdef thingSpeakHandler
             gameStart = data{4};
         end
         
-        function writeScoreIdx(obj, score, idx)
+        function writeScoreIdx(obj, score, idx,round, turn)
             data = obj.readData();
             data{2} = score;
             data{3} = idx;
-            thingSpeakWrite(obj.chID, 'WriteKey', obj.writeKey, data);
+            data{4} = round;
+            data{5} = turn;
+            thingSpeakWrite(obj.chID, 'WriteKey', obj.writeKey,'Values', data);
         end
         
         function [score, idx, gameStart] = readScoreIdx(obj)
@@ -48,21 +57,16 @@ classdef thingSpeakHandler
         function writeGameStart(obj)
             data = obj.readData();
             data{4} = 1;
-            thingSpeakWrite(obj.chID, 'WriteKey', obj.writeKey, data);
-        end
-        
-        function writeGameEnd(obj)
-            data = obj.readData();
-            data{4} = 0;
-            thingSpeakWrite(obj.chID, 'WriteKey', obj.writeKey, data); 
+            data{5} = 1;
+            thingSpeakWrite(obj.chID, 'WriteKey', obj.writeKey, 'Values',data);
         end
     end
     
     methods (Access = private)
         function data = readData(obj)
-            data = thingSpeakRead(obj.chID, 'ReadKey', obj.readKey, 'Fields', 1:4, 'OutputFormat', 'table');
+            data = thingSpeakRead(obj.chID, 'ReadKey', obj.readKey, 'Fields', 1:5, 'OutputFormat', 'table');
             %Get columns 2-5 (everything but time stamps
-            data = data(:, 2:5);
+            data = data(:, 2:6);
             %Return as cell array
             data = table2cell(data);
         end
